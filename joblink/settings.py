2a2014371e2895536fe26f2f939891746e93b189
Buggystepper <linux7506@gmail.com>
2025-08-04 21:18:03 +0300

    Update settings.py

--- joblink/settings.py
+++ joblink/settings.py
@@ -85,9 +85,9 @@ DATABASES = {
     }
 }
 
-DATABASE_URL = os.environ.get('DATABASE_URL')
-if DATABASE_URL:
-    DATABASES['default'] = dj_database_url.parse(DATABASE_URL, conn_max_age=600)
+#DATABASE_URL = os.environ.get('DATABASE_URL')
+#if DATABASE_URL:
+    #DATABASES['default'] = dj_database_url.parse(DATABASE_URL, conn_max_age=600)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
