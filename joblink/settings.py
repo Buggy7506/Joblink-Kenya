36087f2299467a84373669f75d1689d5a0028194
Buggystepper <linux7506@gmail.com>
2025-08-06 01:24:46 +0300

    Update settings.py

--- joblink/settings.py
+++ joblink/settings.py
@@ -97,16 +97,16 @@ WSGI_APPLICATION = 'joblink.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-#DATABASES = {
-    #'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': BASE_DIR / 'db.sqlite3',
-    #}
-#}
-
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+   }
 }
+
+#DATABASES = {
+    #'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+#}
 #DATABASE_URL = os.environ.get('DATABASE_URL')
 #if DATABASE_URL:
     #DATABASES['default'] = dj_database_url.parse(DATABASE_URL, conn_max_age=600)
