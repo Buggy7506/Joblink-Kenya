70fc8c6051421e9489b7d7d8b1c8b9006db7bba9
Buggystepper <linux7506@gmail.com>
2025-08-20 12:32:32 +0300

    Update asgi.py

--- joblink/asgi.py
+++ joblink/asgi.py
@@ -8,19 +8,17 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'joblink.settings')
-
-application = get_asgi_application()
-
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import core.routing
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'joblink.settings')
+
+django_asgi_app = get_asgi_application()  # Keep reference to HTTP app
+
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(core.routing.websocket_urlpatterns)
     ),
