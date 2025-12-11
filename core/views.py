a4feb56605f4f78641971b1720a98ebd01b592eb
Buggystepper <linux7506@gmail.com>
2025-12-11 23:12:42 +0300

    Update views.py

--- core/views.py
+++ core/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages 
 from django.contrib.auth.decorators import login_required, user_passes_test 
 from django.contrib.auth.models import User 
-from django.http import HttpResponse 
+from django.http import HttpResponse  
 from django.template.loader import get_template, render_to_string 
 from django.core.mail import EmailMultiAlternatives 
 from django.utils import timezone 
