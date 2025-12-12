733933fb855352ae0f54ade396c92cb3b3307384
Buggystepper <linux7506@gmail.com>
2025-12-12 21:57:11 +0300

    Update settings.py

--- joblink/settings.py
+++ joblink/settings.py
@@ -200,9 +200,6 @@ DEFAULT_FROM_EMAIL = 'JobLink Kenya <linux7506@gmail.com>'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', 'pk_test_51RvRvyIw0VfmVsTbChuKMjJburhqmWL65VM1qxZ3Az5RYEhMETg5kGZaXH91aK8gosui5RPb7Trr04nus1tE67tD00lBPljhkb')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', 'sk_test_51RvRvyIw0VfmVsTbawUSdB6xKh4RSPtlt4TqLzgNV7u1sWa09mr3XLruD5rq1FinGBmL9XLodixb27p1J3eS3IAz00OfwBFe4V')
 
-# Use SendGrid backend
-EMAIL_BACKEND = "sendgrid_backend.SendgridBackend"
-
 # SendGrid API key from environment variable (recommended)
 SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY")
 
