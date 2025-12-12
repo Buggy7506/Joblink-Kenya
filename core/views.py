4179a7634bda2799a06e4f441753d7fc16fdbbce
Buggystepper <linux7506@gmail.com>
2025-12-12 23:05:30 +0300

    Update views.py

--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404 
+from core.utils import send_verification_email_smtp
 from django.core.mail import send_mail, get_connection
 from django.contrib.auth import login, logout, authenticate 
 from django.contrib import messages 
