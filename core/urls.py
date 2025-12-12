ddf0423adc6cd622350eb032fa2c2a0fded8b987
Buggystepper <linux7506@gmail.com>
2025-12-12 20:06:01 +0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,7 @@ from django.contrib.auth import views as auth_views
 from . import views
  
 urlpatterns = [
+    path('verify-device/', views.verify_device, name='verify_device'),
     path("google/set-password/", views.set_google_password, name="set_google_password"),
     path('google/choose-role/', views.google_choose_role, name='google_choose_role'),
     path("applications/undo/<int:app_id>/", views.undo_delete_application, name="undo_delete_application"),
