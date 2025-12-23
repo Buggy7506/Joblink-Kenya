7549cd89e3b70426825cc0e4d2998b1418b0cd00
Buggystepper <linux7506@gmail.com>
2025-12-23 18:57:42 +0300

    Update urls.py

--- joblink/urls.py
+++ joblink/urls.py
@@ -18,6 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static 
 from django.conf import settings
+from django.http import HttpResponse
+
+def healthcheck(request):
+    return HttpResponse("OK")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
