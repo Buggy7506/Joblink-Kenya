2b67feaa9ff75a88a4021cce5d26afbd657bb765
Buggystepper <linux7506@gmail.com>
2025-12-26 13:33:35 +0300

    Update index.py

--- joblink/api/index.py
+++ joblink/api/index.py
@@ -6,4 +6,4 @@ sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "joblink.settings")
 
-app = get_wsgi_application()
+application = get_wsgi_application()
