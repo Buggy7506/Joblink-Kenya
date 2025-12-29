43812dec98af1dd7c7530bf5369e0d879703fefa
Buggystepper <linux7506@gmail.com>
2025-12-29 13:33:28 +0300

    Update middleware.py

--- joblink/middleware.py
+++ joblink/middleware.py
@@ -3,23 +3,24 @@ from django.shortcuts import redirect
 class WwwAndHttpsRedirectMiddleware:
     """
     Redirects:
-    1. example.com -> www.example.com
+    1. stepper.dpdns.org -> www.stepper.dpdns.org
     2. http:// -> https://
     """
+
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
         host = request.get_host()
-        scheme = 'https' if request.is_secure() else 'http'
         path = request.get_full_path()
+        is_secure = request.is_secure() or request.META.get('HTTP_X_FORWARDED_PROTO') == 'https'
 
-        # Redirect non-www to www
-        if host == "stepper.dpdns.org":
-            return redirect(f"https://www.stepper.dpdns.org{path}", permanent=True)
+        # Desired host
+        desired_host = 'www.stepper.dpdns.org'
+        desired_scheme = 'https'
 
-        # Redirect http to https
-        if scheme == 'http':
-            return redirect(f"https://{host}{path}", permanent=True)
+        # Check if host or scheme need to be fixed
+        if host != desired_host or not is_secure:
+            return redirect(f"{desired_scheme}://{desired_host}{path}", permanent=True)
 
         return self.get_response(request)
