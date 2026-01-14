934529cbba7e3fb04dcb5d3ddcd8ed2e8696592b
Buggystepper <linux7506@gmail.com>
2026-01-14 16:53:34 +0300

    Update views.py

--- core/views.py
+++ core/views.py
@@ -49,6 +49,8 @@ from .forms import (
     CVUploadForm, JobPlanSelectForm, CustomUserCreationForm, ChangeUsernamePasswordForm, AccountSettingsForm, CompanyDocumentForm
 )
 from .utils import send_verification_email, send_whatsapp_otp, send_sms_otp, generate_code, get_client_ip, get_device_fingerprint, is_business_email
+from core.middleware.employer_verified_required import employer_verified_required
+
 
 # Privacy Policy page
 def privacy_policy(request):
