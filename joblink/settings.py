df69456ab41c939eb9c78dd45d335007acf47e92
Buggystepper <linux7506@gmail.com>
2026-01-17 15:55:02 +0300

    Update settings.py

--- joblink/settings.py
+++ joblink/settings.py
@@ -58,6 +58,14 @@ INSTALLED_APPS = [
     'core.apps.CoreConfig',
 ]
 
+INSTALLED_APPS += ["django_celery_results"]
+
+CELERY_BROKER_URL = "redis://localhost:6379/0"
+CELERY_ACCEPT_CONTENT = ["json"]
+CELERY_TASK_SERIALIZER = "json"
+
+CELERY_RESULT_BACKEND = "django-db"
+
 # -----------------------------------------------
 # CLOUDINARY / MEDIA FILE CONFIGURATION
 # -----------------------------------------------
