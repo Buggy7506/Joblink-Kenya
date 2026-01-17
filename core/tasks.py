63d7cd2b094a703849b4e49c7c6c2ff1245a43b9
Buggystepper <linux7506@gmail.com>
2026-01-17 16:04:40 +0300

    Update tasks.py

--- core/tasks.py
+++ core/tasks.py
@@ -4,8 +4,12 @@ from .models import EmployerDocument
 
 @shared_task(bind=True, autoretry_for=(Exception,), retry_kwargs={"max_retries": 5})
 def save_employer_document(self, user_id, file_path, doc_type):
-    EmployerDocument.objects.create(
-        user_id=user_id,
-        document=file_path,
-        document_type=doc_type
-    )
+    from .models import EmployerDocument
+    try:
+        EmployerDocument.objects.get_or_create(
+            user_id=user_id,
+            document=file_path,
+            document_type=doc_type
+        )
+    except Exception as exc:
+        raise self.retry(exc=exc, countdown=10)
