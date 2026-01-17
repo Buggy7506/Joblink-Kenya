5c24ee1fbcd18c3c27fcdfd7721f1f6aef291187
Buggystepper <linux7506@gmail.com>
2026-01-17 16:29:09 +0300

    Update tasks.py

--- core/tasks.py
+++ core/tasks.py
@@ -9,7 +9,7 @@ from django.conf import settings
 User = get_user_model()
 
 @shared_task(bind=True, autoretry_for=(Exception,), retry_kwargs={"max_retries": 5}, retry_backoff=True)
-def save_company_document(self, user_id, temp_file_path, doc_type):
+def save_employer_document(self, user_id, temp_file_path, doc_type):
     """
     Save a CompanyDocument asynchronously via Celery.
     Moves the temp file into Django's media storage and creates the CompanyDocument.
@@ -33,8 +33,9 @@ def save_company_document(self, user_id, temp_file_path, doc_type):
             doc.document.save(os.path.basename(temp_file_path), django_file)
             doc.save()
 
-        # Optionally: remove temp file after saving
+        # Remove temp file after saving
         os.remove(temp_file_path)
 
     except Exception as exc:
+        # Retry automatically after 10 seconds if an error occurs
         raise self.retry(exc=exc, countdown=10)
