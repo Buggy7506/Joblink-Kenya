ecc85d4daaaacda75066d9ac15ac6a43b742b563
Buggystepper <linux7506@gmail.com>
2026-01-19 23:49:45 +0300

    Create brevo_email.py

---
+++ core/brevo_email.py
@@ -0,0 +1,16 @@
+from sib_api_v3_sdk import Configuration, ApiClient, TransactionalEmailsApi, SendSmtpEmail
+from django.conf import settings
+
+def send_brevo_email(subject, html_content, to_email):
+    configuration = Configuration()
+    configuration.api_key['api-key'] = settings.BREVO_API_KEY
+
+    with ApiClient(configuration) as api_client:
+        api_instance = TransactionalEmailsApi(api_client)
+        email = SendSmtpEmail(
+            sender={"name": "Stepper", "email": "no-reply@stepper.dpdns.org"},
+            to=[{"email": to_email}],
+            subject=subject,
+            html_content=html_content,
+        )
+        api_instance.send_transac_email(email)
