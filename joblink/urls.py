86ae5fd4ad09f9d167a620f38d9f622345a27c30
Buggystepper <linux7506@gmail.com>
2026-02-04 15:15:07 +0300

    Update urls.py

--- joblink/urls.py
+++ joblink/urls.py
@@ -35,5 +35,6 @@ urlpatterns = [
     path("robots.txt", robots_txt),
     path('', include('core.urls')),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
