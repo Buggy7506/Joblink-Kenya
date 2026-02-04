5e00b48216155f541c575431b37156afbc87e9cf
Buggystepper <linux7506@gmail.com>
2026-02-04 18:08:22 +0300

    Update settings.py

--- joblink/settings.py
+++ joblink/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "False").lower() == "true"
+DEBUG = os.getenv("DEBUG")
 
 ALLOWED_HOSTS = [
     "www.stepper.dpdns.org",
