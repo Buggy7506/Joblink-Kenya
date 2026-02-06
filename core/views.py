b1bfbd4f580a8c3443a22f522660f8f6910fefe7
Buggystepper <linux7506@gmail.com>
2026-02-06 13:55:23 +0300

    Update views.py

--- core/views.py
+++ core/views.py
@@ -118,7 +118,7 @@ from .forms import (
 from .utils import (
     generate_code,
     send_sms_infini,
-    send_whatsapp_whapi,
+    send_whatsapp_callmebot,
     send_otp,
     get_client_ip,
     get_device_fingerprint,
