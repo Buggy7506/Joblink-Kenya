5778cf3c97b78ad6faef0647120aad107c69a77b
Buggystepper <linux7506@gmail.com>
2026-02-13 23:12:54 +0300

    Create form_alerts.py

---
+++ core/templatetags/form_alerts.py
@@ -0,0 +1,12 @@
+from django import template
+from django.contrib.messages import get_messages
+
+register = template.Library()
+
+
+@register.inclusion_tag("form_errors.html", takes_context=True)
+def render_form_errors(context):
+    request = context.get("request")
+    form = context.get("form")
+    flash_messages = list(get_messages(request)) if request else []
+    return {"form": form, "flash_messages": flash_messages}
