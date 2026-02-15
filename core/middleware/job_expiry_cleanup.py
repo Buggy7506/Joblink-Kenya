add544b6f49fcc5849d0922395b442192d14adc5
Buggystepper <linux7506@gmail.com>
2026-02-15 22:53:04 +0300

    Update job_expiry_cleanup.py

--- core/middleware/job_expiry_cleanup.py
+++ core/middleware/job_expiry_cleanup.py
@@ -1,3 +1,4 @@
+from asgiref.sync import iscoroutinefunction
 from django.utils import timezone
 
 from core.models import Job
@@ -8,9 +9,22 @@ class ExpiredJobCleanupMiddleware:
     Hard-delete expired jobs so they disappear from all surfaces immediately.
     """
 
+    async_capable = True
+    sync_capable = True
+
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        Job.objects.filter(expiry_date__isnull=False, expiry_date__lte=timezone.now()).delete()
+        if iscoroutinefunction(self.get_response):
+            return self.__acall__(request)
+
+        self._cleanup_expired_jobs()
         return self.get_response(request)
+
+    async def __acall__(self, request):
+        self._cleanup_expired_jobs()
+        return await self.get_response(request)
+
+    def _cleanup_expired_jobs(self):
+        Job.objects.filter(expiry_date__isnull=False, expiry_date__lte=timezone.now()).delete()
